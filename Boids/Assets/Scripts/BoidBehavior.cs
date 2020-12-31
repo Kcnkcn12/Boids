12d8f0d7254901cb841dc8e85070fbd4511921e1
Kcnkcn12 <65097025+Kcnkcn12@users.noreply.github.com>
2020-12-30 21:11:03 -0800

    Added Boid Settings Asset and Fixed Interaction Bugs. WARNING: not fully fixed
    - moved boid interaction perameters to a BoidSettings asset
    - added max boid velocity parameter
    - fixed incorrect layer mask detection in scripts

--- Boids/Assets/Scripts/BoidBehavior.cs
+++ Boids/Assets/Scripts/BoidBehavior.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class BoidBehavior : MonoBehaviour
 {
     private CharacterController boid;
-
-    [SerializeField] float detectionDistance = 5.0f;
-
-    [SerializeField] float separationModifier;
-    [SerializeField] float alignmentModifier;
-    [SerializeField] float cohesionModifier;
-
-    [SerializeField] int rayCount;
+    [SerializeField] BoidSettings boidSettings;
 
     private Vector3 resultantMovementVector;
+
     [HideInInspector] public Vector3 MovementVector
     {
         get { return resultantMovementVector; }
@@ -32,6 +26,8 @@ public class BoidBehavior : MonoBehaviour
     void Update()
     {
         resultantMovementVector = BoidIntereaction() + ObstacleInteraction();
+        resultantMovementVector = resultantMovementVector.normalized * Mathf.Clamp(resultantMovementVector.magnitude, 0, boidSettings.maxVelocity);
+//        Debug.Log(resultantMovementVector.magnitude);
         boid.Move(resultantMovementVector * Time.deltaTime);
     }
 
@@ -47,7 +43,7 @@ public class BoidBehavior : MonoBehaviour
     Vector3 BoidIntereaction()
     {
         // obtain array of neighboring boids within a radius around the boid
-        Collider[] neighboringBoids = Physics.OverlapSphere(gameObject.transform.position, detectionDistance, LayerMask.GetMask("Obstacle"));
+        Collider[] neighboringBoids = Physics.OverlapSphere(gameObject.transform.position, boidSettings.detectionDistance, LayerMask.GetMask("Boid"));
 
         Vector3 combinedMovementVector = Vector3.zero; // combination of vectors from all neighboring boids
         if (neighboringBoids.Length == 0)
@@ -69,6 +65,12 @@ public class BoidBehavior : MonoBehaviour
 
             foreach (Collider neighborBoid in neighboringBoids)
             {
+                //ignore self
+                if(neighborBoid.gameObject == gameObject)
+                {
+                    continue;
+                }
+
                 separationVector = Vector3.zero;
                 alignmentVector = Vector3.zero;
                 cohesionVector = Vector3.zero;
@@ -77,15 +79,16 @@ public class BoidBehavior : MonoBehaviour
                 // strength is based on inverse square law ('normalized = vector / magnitude', so only need 1 more "/ magnitude")
                 boidToNeighborVector = neighborBoid.gameObject.transform.position - gameObject.transform.position;
                 neighborAvoidanceVector = -(boidToNeighborVector.normalized / boidToNeighborVector.magnitude);
-                separationVector += neighborAvoidanceVector * separationModifier;
+                Debug.Log(boidToNeighborVector.magnitude);
+                separationVector += neighborAvoidanceVector * boidSettings.separationModifier;
 
                 // alignment rule (move in average same direction and speed as neighboring boids)
                 neighborMovementVector = neighborBoid.gameObject.GetComponent<BoidBehavior>().MovementVector;
-                alignmentVector += neighborMovementVector.normalized * alignmentModifier;
+                alignmentVector += neighborMovementVector.normalized * boidSettings.alignmentModifier;
 
                 // cohesion rule (move toward the average center of neighboring boids)
                 neighborPosition = neighborBoid.gameObject.transform.position;
-                cohesionVector += ((neighborPosition - gameObject.transform.position) / neighboringBoids.Length) * cohesionModifier;
+                cohesionVector += ((neighborPosition - gameObject.transform.position) / neighboringBoids.Length) * boidSettings.cohesionModifier;
 
                 //combine rules
                 combinedMovementVector += separationVector + alignmentVector + cohesionVector;
@@ -121,15 +124,15 @@ public class BoidBehavior : MonoBehaviour
         Vector3 obstacleAvoidanceVector = Vector3.zero;     // modified vector pointing away from obstacle
         Vector3 combinedMovementVector = Vector3.zero;      // combination of vectors from all rays
 
-        for (int i = 0; i < rayCount; ++i)
+        for (int i = 0; i < boidSettings.rayCount; ++i)
         {
             // draw raycast
-            rayAngle = (360 / rayCount) * i;
+            rayAngle = (360 / boidSettings.rayCount) * i;
             //            Debug.DrawRay(transform.position, Quaternion.AngleAxis(rayAngle, transform.up) * transform.forward * detectionDistance);
 
             // move in opposite direction if obstacle is found
             // strength is based on inverse square law ('normalized = vector / magnitude', so only need 1 more "/ magnitude")
-            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(rayAngle, transform.up) * transform.forward, out hit, detectionDistance, LayerMask.GetMask("Boid")))
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(rayAngle, transform.up) * transform.forward, out hit, boidSettings.detectionDistance, LayerMask.GetMask("Obstacle")))
             {
                 boidToObstacleVector = hit.point - gameObject.transform.position;
                 obstacleAvoidanceVector = -(boidToObstacleVector.normalized / boidToObstacleVector.magnitude);
---
+++ Boids/Assets/Scripts/BoidSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BoidSettings : ScriptableObject
+{
+    public float detectionDistance;
+
+    public float separationModifier;
+    public float alignmentModifier;
+    public float cohesionModifier;
+
+    public int rayCount;
+
+    public float maxVelocity;
+}
