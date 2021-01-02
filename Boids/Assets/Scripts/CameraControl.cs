fb61701924e815ff1ba5a1bc040c48fb0a380519
Kcnkcn12 <65097025+Kcnkcn12@users.noreply.github.com>
2021-01-01 16:42:16 -0800

    added simple 2D camera controls
    - added camera movement along x and z axes
    - changed camera angle to point straight down (top-down view)

--- Boids/Assets/Scripts/CameraControl.cs
+++ Boids/Assets/Scripts/CameraControl.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    private float moveX;
+    [SerializeField] float xSpeed;
+    private float moveZ;
+    [SerializeField] float zSpeed;
+
+    private Vector3 movementVector;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,10 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveZ = Input.GetAxis("Vertical") * zSpeed;
+        moveX = Input.GetAxis("Horizontal") * xSpeed;
+
+        movementVector = Vector3.forward * moveZ + Vector3.right * moveX;
+        transform.position += movementVector * Time.deltaTime;
     }
 }
