ffaec08a9a50e8ccbe9e8ce817bd170d1550ae3f
Kcnkcn12 <65097025+Kcnkcn12@users.noreply.github.com>
2021-01-02 18:04:11 -0800

    Added Click to Spawn Boids and Obstacles
    - holding down left mouse button spawns boids
    - holding down right mouse button spawns obstacles
    - changed BoidSpawner/BoidHandler to EntitySpawner/EntityHandler for
    clarity

---
+++ Boids/Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject boidPrefab;
+    [SerializeField] GameObject obstaclePrefab;
+
+    private Ray mouseClickRay;
+    private RaycastHit mouseClickLocation;
+    private Vector3 spawnLocation;
+    private float spawnRotationY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))            // left mouse button
+        {
+            mouseClickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseClickRay, out mouseClickLocation))
+            {
+                // only spawn if clicked on the ground
+                if ((1 << mouseClickLocation.collider.gameObject.layer) == LayerMask.GetMask("Ground"))
+                {
+                    spawnLocation = mouseClickLocation.point;
+                    spawnLocation.y += 1.01f;
+
+                    spawnRotationY = Random.Range(0, 360);
+
+                    Instantiate(boidPrefab, spawnLocation, Quaternion.Euler(0, spawnRotationY, 0));
+                }
+            }
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            mouseClickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseClickRay, out mouseClickLocation))
+            {
+                // only spawn if clicked on the ground
+                if ((1 << mouseClickLocation.collider.gameObject.layer) == LayerMask.GetMask("Ground"))
+                {
+                    spawnLocation = mouseClickLocation.point;
+                    spawnLocation.y += 1.0f;
+
+                    Instantiate(obstaclePrefab, spawnLocation, Quaternion.Euler(Vector3.zero));
+                }
+            }
+        }
+    }
+}
