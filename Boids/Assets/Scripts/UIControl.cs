04d6f958d676237d29215ab4645bfbae0fd176b6
Kcnkcn12 <65097025+Kcnkcn12@users.noreply.github.com>
2021-01-04 22:17:00 -0800

    added sliders for changing boid interaction settings
    - added sliders that change settings
    - added default values that override current values when the app is loaded (or just when it is played in Unity?)

---
+++ Boids/Assets/Scripts/UIControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour
+{
+    [SerializeField] BoidSettings boidSettings;
+
+    [SerializeField] Slider detectiondistanceSlider;
+    [SerializeField] Slider SeparatioSlider;
+    [SerializeField] Slider AlignmentSlider;
+    [SerializeField] Slider CohesionSlider;
+    [SerializeField] Slider ObstacleAvoidanceSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        detectiondistanceSlider.value = boidSettings.detectionDistance;
+        SeparatioSlider.value = boidSettings.separationModifier;
+        AlignmentSlider.value = boidSettings.alignmentModifier;
+        CohesionSlider.value = boidSettings.cohesionModifier;
+        ObstacleAvoidanceSlider.value = boidSettings.obstacleAvoidanceModifier;
+    }
+
+
+    public void UpdateDetectionDistanceModifier(float newValue)
+    {
+        boidSettings.detectionDistance = newValue;
+    }
+
+    public void UpdateSeparationModifier(float newValue)
+    {
+        boidSettings.separationModifier = newValue;
+    }
+
+    public void UpdateAlignmentModifier(float newValue)
+    {
+        boidSettings.alignmentModifier = newValue;
+    }
+
+    public void UpdateCohesionModifier(float newValue)
+    {
+        boidSettings.cohesionModifier = newValue;
+    }
+
+    public void UpdateObstacleAvoidanceModifier(float newValue)
+    {
+        boidSettings.obstacleAvoidanceModifier = newValue;
+    }
+}
